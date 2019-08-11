289e7964be6467a02bee5792de668e4e71218ffd
Craig Rayner <craig@craigrayner.com>
2019-08-12 09:05:34 +1000

    REACT default case error in name.

--- public/dist/Default.34479aab.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["Default"],{"g/IN":function(n,o,w){w("q8RI")},q8RI:function(n,o,w){}},[["g/IN","runtime"]]]);
---
+++ public/dist/default.c0014ee3.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["default"],{"g/IN":function(n,o,w){w("q8RI")},q8RI:function(n,o,w){}},[["g/IN","runtime"]]]);
