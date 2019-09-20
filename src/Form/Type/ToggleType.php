3070b2c438523476f4ef6f86d8638ea3468f00bf
Craig Rayner <craig@craigrayner.com>
2019-09-21 09:36:32 +1000

    Toggle Scripts and Toggle Visible Scripts

--- src/Form/Type/ToggleType.php
+++ src/Form/Type/ToggleType.php
@@ -1,11 +1,29 @@
 <?php
 namespace App\Form\Type;
 
+use App\Manager\ScriptManager;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ToggleType extends AbstractType
 {
+    /**
+     * @var ScriptManager
+     */
+    private $scriptManager;
+
+    /**
+     * ToggleType constructor.
+     * @param ScriptManager $scriptManager
+     */
+    public function __construct(ScriptManager $scriptManager)
+    {
+        $this->scriptManager = $scriptManager;
+    }
+
     /**
      * @return null|string
      */
@@ -21,5 +39,36 @@ class ToggleType extends AbstractType
     public function getParent()
     {
         return HiddenType::class;
+    }
+
+    /**
+     * configureOptions
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+       $resolver->setDefaults(
+           [
+               'visibleByClass' => false,
+               'visibleWhen' => 'Y',
+               'values' => ['Y', 'N'],
+           ]
+       );
+        $resolver->setAllowedTypes('visibleByClass', ['boolean', 'string']);
+        $resolver->setAllowedTypes('visibleWhen', ['string']);
+    }
+
+    /**
+     * buildView
+     * @param FormView $view
+     * @param FormInterface $form
+     * @param array $options
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['visibleByClass'] = $options['visibleByClass'];
+        $view->vars['visibleWhen'] = $options['visibleWhen'];
+        $view->vars['values'] = $options['values'];
+        $this->scriptManager->addPageScript('components/toggle_visibleByClass.html.twig', ['id' => $view->vars['id']]);
     }
 }
