821f2638f928c452630d3b0914e3188ef16e043c
Craig Rayner <craig@craigrayner.com>
2020-04-02 12:25:38 +1100

    Lost some updates
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

--- assets/js/Pagination/PaginationContent.js
+++ assets/js/Pagination/PaginationContent.js
@@ -114,9 +114,9 @@ export default function PaginationContent(props) {
             }
         })
         if (row.actions.length > 0) {
-            columns.push(<td key={'actions'} className={'text-right'}>
+            columns.push(<td key={'actions'}>
                 <div
-                    className="group-hover:flex sm:flex float-right">
+                    className="float-right group-hover:flex sm:flex">
                     {actions}
                 </div>
             </td>)
